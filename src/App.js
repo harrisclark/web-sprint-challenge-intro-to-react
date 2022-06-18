bcb74e620a872f02824f515bcb600e7bc322d2a4
Andrew Clark <andrewclark@Andrews-MacBook-Pro-2.local>
2022-06-17 23:48:56 -0500

    dumpster fire away

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,10 +9,38 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [charData, setCharData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchData, setSearchData] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people/`)
+    .then(res => {
+      console.log(res.data);
+      setCharData(res.data);
+      setSearchData(charData);
+    })
+    .catch(err => console.error(err));
+  }, []);
+
+  useEffect(() => {
+    const searchResults = charData.filter(char => noSpaceToLower(char.name).includes(noSpaceToLower(searchTerm)));
+    searchTerm ? setSearchData(searchResults) : setSearchData(charData);
+  }, [searchTerm, charData])
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
+  const noSpaceToLower = (string) => {
+    return string.split('').filter(l => l.trim()).join('').toLowerCase();
+  } 
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      <input placeholder='Search...' type='text' value={searchTerm} onChange={handleChange} />
+      {searchData.map((char, i) => <Character key={i} char={char} /> )}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,50 @@
 // Write your Character component here
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+
+const CharStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  width: 40%;
+  margin-top: .5rem;
+  border-radius: 5px;
+  background-color: AntiqueWhite;
+
+  p {
+    margin: .2rem;
+  }
+  button {
+    margin-bottom: .5rem;
+  }
+`
+
+const Character = (props) => {
+  const [details, setDetails] = useState(false);
+  const up = '\u2191';
+  const down = '\u2193'
+  
+  const changeDetails = () => {
+    details ? setDetails(false) : setDetails(true)
+  }
+
+  return (
+    <CharStyle>
+      <h3>{props.char.name}</h3>
+      {
+        details &&
+          <>
+            <p>Birth Year: {props.char.birth_year}</p>
+            <p>Gender: {props.char.gender}</p>
+            <p>Weight: {props.char.mass}kg</p>
+          </>
+      }
+      <button onClick={changeDetails}>{details ? up : down}</button>
+
+    </CharStyle>
+  )
+}
+
+export default Character;
